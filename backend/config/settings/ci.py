239e0a3d5a8159c2fc0e6718aca161ab06987c84
Hanseooo <amoguishans@gmail.com>
2025-12-24 14:59:55 +0800

    feat: add ci settings for ci/cd

---
+++ backend/config/settings/ci.py
@@ -0,0 +1,15 @@
+from .base import *
+
+DEBUG = True
+
+# Postgres service from GitHub Actions
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test_db",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
